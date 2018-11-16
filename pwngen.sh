93bd66df3b59bbc2cd1714ed2b9560a11c4f02f0
Ryan Geary <rtgnj42@gmail.com>
2018-11-16 11:56:25 -0500

    Include usage statement and fix filename handling

--- pwngen.sh
+++ pwngen.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+FILENAME="output"
+HELP="false"
+
 for i in "$@"
 do
 case $i in
@@ -14,12 +18,22 @@ case $i in
     -f=*|--filename=*)
     FILENAME="${i#*=}"
     ;;
+    -h|--help)
+    HELP="true"
+    ;;
 esac
 done
 
-echo ${PROCESS}
-FILENAME="output"
-rm ${FILENAME}.py
+if [[ "$HELP" = "true" ]]
+then
+  echo "USAGE:
+    pwngen -f=FILENAME -p=PATH_TO_LOCAL_EXECUTABLE -r=REMOTE_PROCESS -n=REMOTE_PORT_NUMBER"
+  exit
+fi
+
+# if user includes .py remove it, we'll add it later
+FILENAME="${FILENAME%.py}"
+FILENAME="${FILENAME}.py"
 
 # These will _almost_ always be relevent
 echo "
@@ -28,19 +42,22 @@ from Crypto.Util.number import *
 
 context(arch = 'i386', os = 'linux')
 context.log_level = 'debug'
-" >> ${FILENAME}.py
+" > ${FILENAME}
 
 # if remote and port given make a tubes
-echo "r = remote(\"${REMOTE}\", ${PORT})" >> ${FILENAME}.py
+echo "r = remote(\"${REMOTE}\", ${PORT})" >> ${FILENAME}
 
 # if process given redefine r, because you'll probably develop your script
 # locally. Comment later when its ready to pwn.
-echo "r = process(\"$PROCESS\")" >> ${FILENAME}.py
+echo "r = process(\"$PROCESS\")" >> ${FILENAME}
+echo >> ${FILENAME}
 
 # not useful on their own but I always find myself going to find the names
-echo "r.recvuntil(\"\")" >> ${FILENAME}.py
-echo "r.send(\"\")" >> ${FILENAME}.py
+echo "r.recvuntil(\"\")" >> ${FILENAME}
+echo >> ${FILENAME}
+echo "r.send(\"\")" >> ${FILENAME}
+echo >> ${FILENAME}
 
-echo "r.interactive()" >> ${FILENAME}.py
+echo "r.interactive()" >> ${FILENAME}
 
-cat ${FILENAME}.py
+cat ${FILENAME}
