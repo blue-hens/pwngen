aebf5341f8fd5b03b1c823c164692c874cca229b
Ryan Geary <rtgnj42@gmail.com>
2018-11-16 11:33:59 -0500

    Add cmdline parsing

--- pwngen.sh
+++ pwngen.sh
@@ -1,5 +1,25 @@
-filename="output"
-rm $filename.py
+#!/bin/bash
+for i in "$@"
+do
+case $i in
+    -p=*|--process)
+    PROCESS="${i#*=}"
+    ;;
+    -r=*|--remote=*)
+    REMOTE="${i#*=}"
+    ;;
+    -n=*|--port=*)
+    PORT="${i#*=}"
+    ;;
+    -f=*|--filename=*)
+    FILENAME="${i#*=}"
+    ;;
+esac
+done
+
+echo ${PROCESS}
+FILENAME="output"
+rm ${FILENAME}.py
 
 # These will _almost_ always be relevent
 echo "
@@ -8,17 +28,19 @@ from Crypto.Util.number import *
 
 context(arch = 'i386', os = 'linux')
 context.log_level = 'debug'
-" >> $filename.py
+" >> ${FILENAME}.py
 
 # if remote and port given make a tubes
-echo "r = remote($remote, $port)" >> $filename.py
+echo "r = remote(\"${REMOTE}\", \"${PORT}\")" >> ${FILENAME}.py
 
 # if process given redefine r, because you'll probably develop your script
 # locally. Comment later when its ready to pwn.
-echo "r = process($executable)" >> $filename.py
+echo "r = process(\"$PROCESS\")" >> ${FILENAME}.py
 
 # not useful on their own but I always find myself going to find the names
-echo "r.recvuntil(\"\")" >> $filename.py
-echo "r.send(\"\")" >> $filename.py
+echo "r.recvuntil(\"\")" >> ${FILENAME}.py
+echo "r.send(\"\")" >> ${FILENAME}.py
 
-echo "r.interactive()"
+echo "r.interactive()" >> ${FILENAME}.py
+
+cat ${FILENAME}.py
